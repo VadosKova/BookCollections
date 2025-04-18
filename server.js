f4ff86578ddd283ef952c5faec13386a033b840f
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-18 12:57:56 +0300

    Created method for getting book by Id

--- server.js
+++ server.js
@@ -32,3 +32,13 @@ app.get('/books', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ error: 'Book not found' });
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(400).json({ error: 'Incorrect ID' });
+  }
+});
