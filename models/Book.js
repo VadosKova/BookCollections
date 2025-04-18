53d329667bb7650ed1924946b822f004ca5771cb
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-18 13:17:32 +0300

    Fixed bug

--- models/Book.js
+++ models/Book.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: String,
   author: String,
-  available: Boolean, default: true
+  available: { type: Boolean, default: true }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
