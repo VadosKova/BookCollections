ac01cdfa1f5490bff5e93505cdd9c736de4d0e5d
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-18 12:49:37 +0300

    Method for creating book

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ mongoose.connect('mongodb+srv://admin:admin@db.bdboune.mongodb.net/?retryWrites=
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
+
+app.post('/books', async (req, res) => {
+  try {
+    const { title, author, available } = req.body;
+    if (!title || !author) {
+      return res.status(400).json({ error: 'title и author обязательные поля' });
+    }
+    const book = await Book.create({ title, author, available });
+    res.status(201).json(book);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
